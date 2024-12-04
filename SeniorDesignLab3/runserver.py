3e088a2c531828c8508efda5284b3816ef82a05b
Mikloultra <mbwest@uiowa.edu>
2024-12-03 21:46:57 -0600

    fuck elastic beanstalk

--- SeniorDesignLab3/runserver.py
+++ SeniorDesignLab3/runserver.py
@@ -9,8 +9,8 @@ from app import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '8080'))
+        PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 8080
+        PORT = 5555
     app.run(HOST, PORT)
 
